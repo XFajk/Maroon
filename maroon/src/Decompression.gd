8e0156a52c8ce758f421d4310f6975d03c206bc8
Jozef Šifra <70467972+juzek32@users.noreply.github.com>
2024-10-23 15:53:37 +0200

    decompression basics work
    further progress on decompression room, created some bugs but will fix soon. Almost done!

--- maroon/src/Decompression.gd
+++ maroon/src/Decompression.gd
@@ -4,9 +4,13 @@ extends Node3D
 @onready var outside_door = $MainDoor
 
 @onready var decomp_timer = $DecompTimer
+@onready var unlocked_timer = $UnlockedTimer
 
 var is_decompressing: bool = false
 @export var decompressing_time = 5 #seconds
+@export var unlocked_time = 7 #seconds
+
+var entering: bool #used to determine which door to open after decompressing
 
 func _process(delta: float) -> void:
 	pass
@@ -15,10 +19,26 @@ func _process(delta: float) -> void:
 func _on_button_pressed(button_name) -> void:
 	if button_name == "outside":
 		outside_door.is_locked = false
+		entering = true
 	if button_name == "inside":
 		inside_door.is_locked = false
+		entering = false
 	if button_name == "decomp":
 		inside_door.is_locked = true
 		outside_door.is_locked = true
 		is_decompressing = true
 		decomp_timer.start(decompressing_time)
+
+
+func _on_decomp_timer_timeout() -> void:
+	is_decompressing = false
+	if entering:
+		inside_door.is_locked = false
+	else:
+		outside_door.is_locked = false
+	unlocked_timer.start(unlocked_time)
+
+
+func _on_unlocked_timer_timeout() -> void:
+	outside_door.is_locked = true
+	inside_door.is_locked = true
