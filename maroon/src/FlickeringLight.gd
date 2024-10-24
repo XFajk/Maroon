496d5b874d156c38ab688faed0f7c5b36411472e
rudolfvrbensky <ertyperty24@gmail.com>
2024-10-24 23:14:09 +0200

    added spark fire and smoke particles

--- maroon/src/FlickeringLight.gd
+++ maroon/src/FlickeringLight.gd
@@ -1,17 +1,29 @@
 extends Node3D
 
-@export_range(0, 100) var flickering_frequency: float
+@export_range(0, 100) var chance_of_flickering: float
 @export var off_value: float = 0.2
+@export var off_bar_value: float = 0.0
 var on_value: float
+var on_bar_value: float = 16
+
+@export_range(0, 100) var chance_of_sparks: float
 
 @onready var LightBar = $Cylinder
+var LightBarShader: StandardMaterial3D
 @onready var Light = $Light
+
+var Sparks = preload("res://vfx/Sparks.tscn")
 
 func _ready() -> void:
 	on_value = Light.light_energy
+	LightBarShader = LightBar.get_surface_override_material(0)
 
 func _physics_process(delta: float) -> void:
-	if flickering_frequency > randi_range(0, 100):
+	if chance_of_flickering > randi_range(0, 100):
 		Light.light_energy = off_value
+		LightBarShader.emission_energy_multiplier = off_bar_value
+		if chance_of_sparks > randi_range(0, 100):
+			add_child(Sparks.instantiate())
 	else:
 		Light.light_energy = on_value
+		LightBarShader.emission_energy_multiplier = on_bar_value
