a31e1359d10cfae82f62ee730f533ce6ed508cfc
Jozef Šifra <70467972+juzek32@users.noreply.github.com>
2024-10-26 20:34:47 +0200

    Fixed the warehouse colliders, added cube texture, started with base alpha furniture
    some work in main that probably should have been done in branches but this time its ok :)

--- maroon/src/FlickeringLight.gd
+++ maroon/src/FlickeringLight.gd
@@ -18,6 +18,7 @@ func _ready() -> void:
 	visible = true
 	on_value = Light.light_energy
 	LightBarShader = LightBar.get_surface_override_material(1)
+	print(LightBarShader)
 
 func _physics_process(delta: float) -> void:
 	if chance_of_flickering > randi_range(0, 100):
