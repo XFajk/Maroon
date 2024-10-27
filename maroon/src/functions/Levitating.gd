92c1bf9c5fbe2385696da0d7d9f22040411c66f8
Jozef Šifra <70467972+juzek32@users.noreply.github.com>
2024-10-27 10:54:36 +0100

    REORGANIZATION
    moved files and made folders...

---
+++ maroon/src/functions/Levitating.gd
@@ -0,0 +1,18 @@
+extends StaticBody3D #do not put @tool it breaks it
+
+@export var speed = 0.05
+@export var lev_range = 0.1 #meters
+var progress = 0
+var count = 0
+var original_position
+
+func _ready() -> void:
+	randomize()
+	count = randf()
+	original_position = position
+
+func _process(delta: float) -> void:
+	count += speed
+	progress = cos(count) / 2 + 1
+	
+	position = original_position + progress * Vector3.DOWN * lev_range
