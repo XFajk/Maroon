d3b703b247477859e59a2f7d68ca39f74212672a
rudolfvrbensky <ertyperty24@gmail.com>
2024-10-30 20:35:07 +0100

    added some sounds

--- maroon/src/2d_scenes/LogSystemTemplateButton.gd
+++ maroon/src/2d_scenes/LogSystemTemplateButton.gd
@@ -6,7 +6,10 @@ extends Button
 @export var Date: Label = null
 @export var LogContent: RichTextLabel = null
 
+@onready var SelectSound: AudioStreamPlayer = $SelectSound
+
 func _pressed() -> void:
+	SelectSound.play()
 	Name.text = log.title
 	Date.text = log.date
 	LogContent.text = log.content_of_log
