6ae2b0131437e347b520f076909ae445f8e938f1
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2024-11-02 21:08:13 +0100

    finished way points

--- maroon/src/objects/JarryCan.gd
+++ maroon/src/objects/JarryCan.gd
@@ -3,12 +3,18 @@ extends StaticBody3D
 @export var downloadeble_positions: Array[Node3D]
 @export var outlines: Array[MeshInstance3D]
 
+@export var voice_line: AudioStreamPlayer = null
+@export_multiline var voice_line_line: String = ""
+
 var deleted = false
 
 func _ready() -> void:
 	stop_showing_interaction()
 
 func interact(player: CharacterBody3D) -> void:
+	player.voice_line = voice_line
+	player.voice_line_line = voice_line_line
+	player.say_voice_line()
 	
 	for pos in downloadeble_positions:
 		if pos == null:
